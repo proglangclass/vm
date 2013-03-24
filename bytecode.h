c08fc14cfa7d5ec11a321af833b2109e548ea9eb
macournoyer <macournoyer@gmail.com>
2013-03-24 16:48:33 -0400

    Compile simple.rb

--- bytecode.h
+++ bytecode.h
@@ -2,18 +2,12 @@
   This file was compiled by the `compile` script.
   It contains the bytecode for the following code:
   
-    print("the answer is:")
-    a = 30 + 2
-    if true
-      print(a)
-    end
+    print("hi")
 */
 
 #define LITERALS { \
-    (void *) "the answer is:", \
+    (void *) "hi", \
     (void *) "print", \
-    (void *) 30, \
-    (void *) 2, \
   }
 
-#define INSTRUCTIONS { 3, 2, 0, 0, 1, 1, 1, 2, 1, 3, 10, 7, 0, 5, 1, 8, 6, 3, 6, 0, 0, 1, 1, 11 }
+#define INSTRUCTIONS { 3, 2, 0, 0, 1, 1, 11 }
